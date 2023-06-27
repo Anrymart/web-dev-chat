09457f0d780037206e40b30ef970fea997ed44ac
Anrymart <anrymart@users.noreply.github.com>
2023-06-27 10:55:21 +0200

    Adds Dockerfile.

--- src/main/java/app/util/HerokuUtil.java
+++ src/main/java/app/util/HerokuUtil.java
@@ -7,6 +7,6 @@ public class HerokuUtil {
     if (processBuilder.environment().get("PORT") != null) {
       return Integer.parseInt(processBuilder.environment().get("PORT"));
     }
-    return 7070;
+    return 7000;
   }
 }
